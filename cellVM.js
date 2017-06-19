05e104ff304692031c81d146050c9ba2bc78f9c8
Dario Cangialosi, Coder <dario.cangialosi@gmail.com>
2017-06-20 01:45:05 +0200

    test program + better debuggability

--- cellVM.js
+++ cellVM.js
@@ -4,6 +4,18 @@ Virtual Machine that is integer-numbers-based(byte-based) and self-modifying.
 
 https://docs.microsoft.com/en-us/dotnet/standard/io/memory-mapped-files
 */
+
+function log(data, optional_prefix){
+	const debug_enabled = true
+	if(debug_enabled){
+		data = JSON.stringify(data)
+		if(optional_prefix != undefined){
+			data = optional_prefix + ': ' + data
+		}
+		console.log(data)
+	}
+}
+
 var description = 'Virtual Machine that is integer-numbers-based(byte-based) and self-modifying.'
 console.log(description)
 
@@ -14,20 +26,36 @@ var max_index = max_integer
 
 // array initialization
 var bytes = [] // 225 is max integer for a byte
-var array = bytes // array of bytes: 225 is max integer for a byte
+var bytes_decrement_by_3 = [3,4,5, 6,7,8, 0,1,0] // 225 is max integer for a byte
+var array = bytes_decrement_by_3 // array of bytes: 225 is max integer for a byte
 
 run() // run after intialization
 
 function run(){
 
-current(0) // initialize current
-
 while(current()<max_index){
+	
+	log(current(), '@current')
+	
+	/*
 	// integers increment with modulo (indirect indexing with triplets of contiguous integers)
 	integers_set(integers(current()+0),
 	( integers(integers(current()+0)) + integers(integers(current()+1)) ) % max_integer_modulo )
 
 	current_set( integers(integers(current()+2)) )
+	*/
+	// integers increment with modulo (indirect indexing with triplets of contiguous integers)
+	
+	var accumulator_index = integers(current()+0)
+	var accumulator_value_before = integers(accumulator_index)
+	var delta = integers(integers(current()+1))
+	var accumulator_value_after = ( accumulator_value_before + delta ) % max_integer_modulo
+	integers_set(accumulator_index, accumulator_value_after)
+
+	var next = integers(integers(current()+2))
+	current_set( next )
+
+	log({accumulator_index, accumulator_value_before, delta, accumulator_value_after, next}, 'after step dump')
 }
 
 console.log('no more running')
